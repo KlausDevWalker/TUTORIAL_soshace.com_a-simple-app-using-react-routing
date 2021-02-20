abedfbc72f0a1df9962d9171c7ae6784dd947bc5
Klaus Ferreira <klaus@codewalkers.org>
2021-02-20 00:37:33 -0300

    add source-code files and formatting

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Contact() {
+	return (
+		<div className="contact">
+			<p>
+				You can contact me via Ghost mail. Don't know what is it? Then you are
+				not a ghost like me.
+			</p>
+		</div>
+	);
+}
